f021c0adf798b6e357e698330acf279a9b5a2fce
hroft <sagdinov.a@yandex.ru>
2019-01-26 00:54:57 +0300

    add docker docker-comose.yml for testing build project

---
+++ docker/src/precache-manifest.90808e0a6e7aeb1cc39db0370d3928dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "693dbe7842d8df5a328f",
+    "url": "/static/js/main.693dbe78.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "693dbe7842d8df5a328f",
+    "url": "/static/css/main.1b28e85a.chunk.css"
+  },
+  {
+    "revision": "c3527797aa256f4479c1789a5778debb",
+    "url": "/index.html"
+  }
+];
